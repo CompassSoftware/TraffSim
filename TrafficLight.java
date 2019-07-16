ffc8de6847823b8cc1f788b02a6dfe67ff1840fe
Monroe Jackson <jacksonmd1@appstate.edu>
2019-07-16 12:32:42 -0400

    initial commit

---
+++ TrafficLight.java
@@ -0,0 +1,12 @@
+/*
+ * TrafficLight.java
+ *
+ * Contains discrete states for different traffic light colors.
+ * Sends this information to Controller and accelerates changes based 
+ * on what controller commands.
+ */
+
+public class TrafficLight{
+
+
+}
