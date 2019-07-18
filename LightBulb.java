f315f884650af229217e34a4d4e5fce7c47ca359
Monroe Jackson <jacksonmd1@appstate.edu>
2019-07-18 11:49:29 -0400

    fixed errors, bulbs now integrated

---
+++ LightBulb.java
@@ -0,0 +1,27 @@
+/*
+ * LightBulb.java
+ *
+ * Contains a small amount of data, representing an individual
+ * light bulb in a traffic light chasis
+ *
+ */
+
+
+public class LightBulb{
+
+    //true == on
+    //false == off
+    private boolean state;
+
+    public boolean getState(){
+        return state;
+    }
+
+    public void setState(boolean state){
+        this.state = state;
+
+    }
+
+
+
+}
