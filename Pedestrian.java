9038fe9e645b96b00185b785673a07ed559af00a
James Brown <brownje3@appstate.edu>
2019-07-21 20:39:44 -0400

    Pedestrian.java done

--- Pedestrian.java
+++ Pedestrian.java
@@ -1,5 +1,52 @@
 public class Pedestrian
 {
+    private boolean motion;
+    private boolean wait;
+    private string location;
 
+    public Pedestrian(String loc)
+    {
+        motion = false;
+        wait = true;
+        location = loc;
+    }
+    
+    public void stop ()
+    {
+        setMotion(false);
+        setWait(true);
+        System.out.println("Pedestrian has stopped at the " + location  + " crosswalk");
+    }
 
+    public void go()
+    {
+        setMotion(true);
+        setWait(false);
+        System.out.println("Pedestrian crosses at  the " + location + " crosswalk");
+    }
+    
+    public boolean getMotion()
+    {
+        return motion;
+    }
+
+    public void setMotion(boolean bool)
+    {
+        motion = bool;
+    }
+
+    public boolean getWait()
+    {
+        return wait;
+    }
+
+    public void setWait(boolean bool)
+    {
+        wait = bool;
+    }
+
+    public String getLocation()
+    {
+        return locaiton;
+    }
 }  
