65a3322ffa360e9132080a555dfbb560a4ac491c
Monroe Jackson <jacksonmd1@appstate.edu>
2019-07-23 12:14:22 -0400

    capitalized Sensor and SensorTest

---
+++ SensorTest.java
@@ -0,0 +1,30 @@
+/*
+//import org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class sensorTest {
+   
+    sensor sense;
+
+    @BeforeEach
+    void init() {
+        sense = new sensor();
+    }
+
+    @Test
+    public void testCarSensor() {
+        
+        assertTrue(sense.CarSensor(true));
+
+    }
+
+    @Test
+    public void testPedSensor() {
+        assertTrue(sense.Pedestrian(true));
+    }
+
+
+}
+*/
