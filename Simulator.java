b224cded20f8a5e1db912376d3c746c405a992fb
Monroe Jackson <jacksonmd1@appstate.edu>
2019-07-24 14:34:33 -0400

    put documented pseudocode in simulator

--- Simulator.java
+++ Simulator.java
@@ -31,65 +31,42 @@ public class Simulator{
         System.out.println("\t\t***Simulation Concluded***");
     }
 
-    /*
-     * East car approaches light, delayed by minTimer
-     */
-    public static void run2(String[] args){
-        System.out.println("\t\t***This is a Simulation of an Eastbound Car moving into a 4-way intersection***");
-        Lane[] lanes = new Lane[4];
-        for (int i = 0; i < lanes.length; i++) lanes[i] = new Lane('N');
-        lanes[1].setTag('W');
-        lanes[2].setTag('S');
-        lanes[3].setTag('E');
-        Car eastCar = new Car(lanes[3]); // Eastbound Lane
-        lanes[3].queue.add(eastCar);
-        Car northCar = new Car(lanes[0]);
-        lanes[0].queue.add(northCar);
-        Controller intersectControl = new Controller(lanes);
-        lanes[0].setLight('G');
-        lanes[2].setLight('G');
-
-         northCar.stop();
-            int[] lanesWithCar = intersectControl.lanesWithCar();
-            for (int laneToSend : lanesWithCar)
-                if (laneToSend != -1) intersectControl.sendCar(laneToSend);
-            northCar.go();
-
-        eastCar.stop();    
-
-        while(minTimer > 0 && maxTimer > 0){
-            intersectControl.printLights();
-
-           
+    public static void run (String[] args){
 
 
+        /*
+         * To model different scenarios, we can just change the amount of cars and
+         * the lanes they're in, but the logic below should (once finished) be run for
+         * all the scenarios.
+         */
+        int i = 0;
+        int minTimer;
+        //globaltimer to stop program, otherwise runs regardless of the amount of cars remaining
+        while(globaltimer > 0){
+            
+            /*
+             * inner loop represents the ticks in a single light CYCLE. 
+             * Exiting this loop means that the green lights
+             * will be made red and the red lights will be made green
+             *
+             * REQUIRES: some method of switching to the next lane
+             */
+            while(minTimer > 0 && maxTimer > 0){
+                intersectControl.printLights();             //always show the status of the lights
+                if (head.peek() != null){                   //checks if there is a car at the front of the list
+                    head.go();                              //car drives thru intersection and leaves
+                    minTimer.setTime(MINTIME);              //reset mintimer because a car triggered the sensor in its lane
+                }
+                else
+                    minTimer.tick();                        //tick minTimer only when a car does not trigger the head sensor
 
-        }
-
-
+                maxTimer.tick();    
+                carList.advance();                          //Always tick maxtimer and advance the carlist
+            }
+                    }
     }
 
-/*
- * No cars, maxTimer goes off and cycles light
- */
-    public static void run3(String[] args){
-        System.out.println("\t\t***This is a Simulation of an Eastbound Car moving into a 4-way intersection***");
-        Lane[] lanes = new Lane[4];
-        for (int i = 0; i < lanes.length; i++) lanes[i] = new Lane('N');
-        lanes[1].setTag('W');
-        lanes[2].setTag('S');
-        lanes[3].setTag('E');
-        Controller intersectControl = new Controller(lanes);
-        lanes[0].setLight('G');
-        lanes[2].setLight('G');
 
-        while(minTimer > 0 && maxTimer > 0){
-          intersectControl.printLights(); 
-        }
-
-        System.out.println("\t\t***One cycle completed***\n\t\t***Simulation Concluded***");
-
-    }
 }
 
 
