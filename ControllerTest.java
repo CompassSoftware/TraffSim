b0faacc951486aedd715b1079b35f687b4aec48f
James Brown <brownje3@appstate.edu>
2019-07-24 10:29:50 -0400

    Controller commented again

--- ControllerTest.java
+++ ControllerTest.java
@@ -12,20 +12,31 @@ public class ControllerTest {
       static Controller controller;
       static TrafficLight light;
       static Lane[] lanes;
-      
+      static Car car;
+
       @BeforeEach
       void makeController()
       {
         lanes = new Lane[1];
         lanes[0] = new Lane('E');
+        car = new Car(lanes[0])
         controller = new Controller(lanes);
         light = new TrafficLight();
       }
 
       @Test
-      public void testSenseNotify() 
+      public void testlanesWithCar() 
       {
-	    assertTrue(controller.isSenseNotify());
+        int[] j;
+        boolean b = false;
+        lanes[0].addCar(car);
+
+        j = controller.lanesWithCar();
+        for (int i : j) {
+            if (j[i] != 0)
+        }
+
+        assertTrue();
       }
       
       @Test
