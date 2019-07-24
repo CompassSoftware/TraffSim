8c7ec3b88fa7da258a81798db1b473fe3219ed94
James Brown <42588084+brownje3@users.noreply.github.com>
2019-07-24 03:05:13 -0400

    Checkstyle for Car

--- Car.java
+++ Car.java
@@ -1,110 +1,114 @@
-public class Car
-{
+/**
+ * 
+ * @author James Brown
+ *
+ * @version 1.0
+ *
+ */
+public class Car {
     private boolean motion;
     private boolean onSensor;
     private Lane lane;
     
-    /*
+    /**
      *  Car constructor takes a lane object and returns a car object.
      *
      *  @param lane - Lane object where the car approaches
      */
-    public Car(Lane lane)
-    {
+    public Car(Lane lane) {
         motion = true;
         onSensor = false;
         this.lane = lane;
-        System.out.println("The car approaches the " + lane.getTag() + " lane of the intersection.");
+        System.out.println("The car approaches the "
+        		+ lane.getTag() + " lane of the intersection.");
     }
     
-    /*
+    /**
      *  Stop orders the car to stop moving and prints where it stops.
      */
-    public void stop()
-    {
-        if (lane.getLight() != 'G'){
+    public void stop() {
+        if (lane.getLight() != 'G') {
             setMotion(false);
             setSensor(true);
-            System.out.print("The car has stopped at the " + lane.getTag() + " intersection. ");
-            System.out.println("The " + lane.getTag() + " Sensor has been notified");
+            System.out.print("The car has stopped at the " 
+            		+ lane.getTag() + " intersection. ");
+            System.out.println("The " + lane.getTag() 
+            	+ " Sensor has been notified");
         }
     }
-    /*
+    
+    /**
      *  Go orders the car to leave the intersection and tells where it goes.
      */
-    public void go()
-    {
-        if (lane.getLight() == 'G'){
+    public void go() {
+        if (lane.getLight() == 'G') {
             setMotion(true);
             setSensor(false);
-            System.out.println("The car leaves the " + lane.getTag()  + " intersection.");
+            System.out.println("The car leaves the " 
+            		+ lane.getTag()  + " intersection.");
             lane.queue.remove();
         }
     }
 
-    /*
+    /**
      *  getTag returns the direction of the lane.
      *
      *  @return direction of lane car is in
      */
-    public char getTag()
-    {
+    public char getTag() {
         return lane.getTag();
     }
 
-    /*
+    /**
      *  getMotion returns true or false if car is in motion.
      *
      *  @return t/f
      */
-    public boolean getMotion()
-    {
+    public boolean getMotion() {
         return motion;
     }
-    /*
+    
+    /**
      *  setMotion is set to t/f depending if the car has stopped or gone.
+     *  
+     *  @param bool - t/f for motion field
      */
-    public void setMotion(boolean bool)
-    {
+    public void setMotion(boolean bool) {
         motion = bool;
     }
 
-    /*
-     *  returns t/f depending if the car is on a sensor
+    /**
+     *  returns t/f depending if the car is on a sensor.
      *
      *  @return t/f
      */
-    public boolean getSensor()
-    {
+    public boolean getSensor() {
         return onSensor;
     }
-    /*
-     *  setSensor is used when a car approaches or leaves an intersection
+    /**
+     *  setSensor is used when a car approaches or leaves an intersection.
      *
      *  @param bool - true or false for when it's on a Lane sensor
      */
-    public void setSensor(boolean bool)
-    {
+    public void setSensor(boolean bool) {
         onSensor = bool;
     }
 
-    /*
+    /**
      *  returns the lane object.
      *
      *  @return this car's Lane object
      */
-    public Lane getLane()
-    {
+    public Lane getLane() {
         return lane;
     }
     
-    /*
+    /**
      *  setLane sets the lane for the car.
      *
      *  @param lane - the lane where the car is at.
      */
-    public void setLane(Lane lane)
-    {
+    public void setLane(Lane lane) {
         this.lane = lane;
     }
 }
